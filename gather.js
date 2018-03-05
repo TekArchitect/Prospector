2955fea5bbd190ac6bffc2f674c1c2c2b0efa84d
TekArchitect <35783474+TekArchitect@users.noreply.github.com>
2018-03-05 16:05:13 -0600

    Create gather.js

---
+++ gather.js
@@ -0,0 +1,21 @@
+// scrape data for an NBA season
+// organize data for processing
+// finalize processing of data
+
+// create object called sport
+// each sport contains teams
+// NFL contains 16 teams
+// MLB contains _ teams
+// NBA contains 32 teams
+// NHL contains _ teams
+
+var sport = ["NBA", "MLB", "NFL", "NHL"]{
+  var team = "";
+  };
+
+var season = 2017;
+ 
+console.log("Welcome To Prospector 1.0");
+console.log("the premier sports data analytics website");
+
+
